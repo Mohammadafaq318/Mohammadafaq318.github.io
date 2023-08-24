9ffb28d6550569b0d4bb8676a65ffebe9430f717
moaf <mafaqq318@gmail.com>
2023-08-24 18:37:13 +0300

    Using EJS Express

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app=express();
+const port=3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+  })
+
+app.get('/contact-me', (req, res) => {
+res.render('contact-me.ejs');
+})
+
+
+app.listen(port,()=>{
+    console.log("Server running on "+port);
+});
